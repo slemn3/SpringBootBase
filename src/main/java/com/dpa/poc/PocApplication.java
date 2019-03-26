ec00f14e852b4e872a3566d518643806ff5453eb
X559335 <scott.s.lem@kp.org>
2019-03-26 15:32:20 -0700

    intial

---
+++ src/main/java/com/dpa/poc/PocApplication.java
@@ -0,0 +1,14 @@
+package com.dpa.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HEllos");
+		SpringApplication.run(PocApplication.class, args);
+	}
+
+}
